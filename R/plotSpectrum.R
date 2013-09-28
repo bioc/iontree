7284959e5f5fff29fbbd9d82e565fec1a22b0c0b
m.cao <unknown>
2013-09-28 04:54:40 +0000

    can plot two numeric vectors or one two-column matrix
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/iontree@80872 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotSpectrum.R
+++ R/plotSpectrum.R
@@ -1,16 +1,26 @@
 plotSpectrum <-
-function (x, y, top = 20, type = "h", scale100 = FALSE, digit.label = 0, 
-                       col="black", pos = 0, main = "", clickAddLabels=FALSE, ...) {
-    if (scale100 == TRUE) y = y/max(y) * 100
+function (x, y=NULL, top = 20, type = "h", scale100 = FALSE, digit.label=1, 
+    col = "black", pos = 0, main = "", clickAddLabels = FALSE, ...) {
+    if(is.null(y)){
+      y=x[,2]
+      x=x[,1]
+    }
+    if (scale100 == TRUE) 
+        y = y/max(y) * 100
     tmp = topIons(x, y, top)
     x1 = tmp[, 1]
     y1 = tmp[, 2]
-    plot(x, y, type = type, main = main, xlab = "m/z", ylab = "intensity", ...)
-    mzLabels=as.character(round(x1[1:top], digit.label))
-    if(!clickAddLabels){
-      text(x1[1:top], y1[1:top] + pos, mzLabels, col = col, cex = 0.8)
-    }else{
-      identify(x1[1:top], y1[1:top] + pos, labels=mzLabels, col=col)  
+    plot(x, y, type = type, main = main, xlab = "m/z", ylab = "intensity", 
+        ...)
+    mzLabels = as.character(round(x1[1:top], digit.label))
+    if (!clickAddLabels) {
+        text(x1[1:top], y1[1:top] + pos, mzLabels, col = col, 
+            cex = 0.8)
+    }
+    else {
+        identify(x1[1:top], y1[1:top] + pos, labels = mzLabels, 
+            col = col)
     }
 }#>>>
+
 
